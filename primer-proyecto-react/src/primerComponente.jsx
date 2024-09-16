46db6de0ebc6c450398d0077bb090b8eeb3bc2f4
Nicolas Lacave <Nicolas.Lacave@belltech.la>
2024-09-16 13:01:58 -0300

    Primer hola mundo

---
+++ primer-proyecto-react/src/primerComponente.jsx
@@ -0,0 +1,28 @@
+/* function PrimerComponente() {
+    return ( <h1>Hola Mundo</h1> );
+}
+
+export default PrimerComponente;
+*/
+
+/*
+import React, { Component } from 'react';
+
+class PrimerComponente extends Component {
+    state = {  } 
+    render() { 
+        return ( <h1>Hola Mundo</h1> );
+    }
+}
+ 
+export default PrimerComponente;
+
+*/
+
+import React from 'react'
+
+export const PrimerComponente = () => {
+  return (
+    <h1>Hola Mundo</h1>
+  )
+}
